fa64e3cd18da3e0642c148f64d8009a0bf5095f7
heeba <heebahassan182@gmail.com>
2023-10-08 17:37:54 +0300

    intial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+document.getElementById("heading").innerText = "Welcome to our page!";
+// document.getElementById("text").innerText = "Name";
+document.getElementById("submit").innerText = "Sign in";
+document.getElementById("heading2").innerText = "Log into your account";
+// document.getElementsByClassName("body").style.backgroundColor = "floral white"
+
+
+document.getElementById("heading").style.color = "red";
+document.getElementById("heading").style.fontSize = "40px";
+
+document.getElementById("heading2").style.fontSize = "20px";
+
+document.getElementById("text").style.width = "20%";
+document.getElementById("text").style.padding = "12px";
+document.getElementById("text").style.borderRadius = "20px";
+document.getElementById("text").style.border = "1px solid gray";
+// document.getElementById("text").style.borderColor = "gray";
+
+document.getElementById("password").style.width = "20%";
+document.getElementById("password").style.padding = "12px";
+document.getElementById("password").style.borderRadius = "20px";
+document.getElementById("password").style.border = "1px solid gray";
+
+document.getElementById("submit").style.width = "10%";
+document.getElementById("submit").style.padding = "5px";
+document.getElementById("submit").style.backgroundColor = "red";
+document.getElementById("submit").style.borderRadius = "20px";
+document.getElementById("submit").style.border = "1px solid white";
+document.getElementById("submit").style.fontWeight = "600";
+document.getElementById("submit").style.color = "white";
